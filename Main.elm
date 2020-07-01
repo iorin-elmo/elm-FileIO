62bacc639de0f9e4a18c3e45557159e2d07240ef
iorin-elmo <iorinxxxx@gmail.com>
2020-07-01 22:28:34 +0900

    file selector

---
+++ Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, div, text, br, button)
+import Html.Events exposing (onClick)
+import File exposing (File)
+import File.Select as Select
+import Task
+
+type alias Model =
+  { fileName : String
+  , fileText : String
+  }
+
+initModel =
+  { fileName = ""
+  , fileText = ""
+  }
+
+type Msg
+  = Pressed
+  | Open String
+  | SelectFile File
+
+
+--Update--
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    Pressed ->
+      ( model
+      , Select.file [model.fileName] SelectFile
+      )
+    Open str ->
+      ( { model
+        | fileText = str
+        }
+      , Cmd.none
+      )
+    SelectFile file ->
+      ( model
+      , Task.perform Open (File.toString file)
+      )
+
+--View--
+
+view : Model -> Html Msg
+view model =
+  div[]
+    [ button
+      [ onClick Pressed ]
+      [ text "Select File" ]
+    , br[][]
+    , text model.fileText
+    ]
+
+--Main--
+
+main : Program () Model Msg
+main =
+  Browser.element
+    { init = \_-> (initModel, Cmd.none )
+    , view = view
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    }
